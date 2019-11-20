4b30e7e5b8d9950efd6cbc72cd457a21ab3474c7
Manzi Joseph <manzeyjoseph@gmail.com>
2019-11-20 12:47:26 +0000

    yes finally my yelp API is pulling out data

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import './search.css'
 import axios from 'axios';
 
 
-class search extends Component {
-    
-    render(){
-        axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${locationSearched}`, {
+class Search extends Component {
+  state = {};
+  
+  render() {
+    const locationSearched = 'Waterford';
+    const REACT_APP_API_KEY = 'AxP4BGuBsV4xy1lNws9OIxvJ0ZpVxmXpu4yDz2lNmkXEAPNeIxO9uhPLKHudSmJpQ6HONbd8-iCqpUxvKSalQDbStWsIm1fMaQRlHJ7qWrymAsMQQ8qm7S1STRHBXXYx';
+
+    axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${locationSearched}`, {
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    //Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+    Authorization: `Bearer ${REACT_APP_API_KEY}`
 },
   params: {
   categories: 'breakfast_brunch',
@@ -19,11 +25,18 @@ console.log(res)
 .catch((err) => {
 console.log ('error')
 })
-        return{
 
-        }
+    return(
+      <div>
+        <h1>Welcome to another version of google</h1>
+        <input name="text" type="text" placeholder="Search"></input>
+      </div> 
+
+    )
+
     }
 }
-
-export default search;
+export default Search;
+    
+  
     
