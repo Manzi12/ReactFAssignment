b364b324e1461c84844880e93af9005c7fa9ad77
Manzi Joseph <manzeyjoseph@gmail.com>
2019-11-21 21:47:07 +0000

    signup form template

---
+++ src/components/signup/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+const SignUpPage = () => (
+    <div>
+        <h1>SignUp</h1>
+        <SignUpForm />
+    </div>
+);
+
+class SignUpForm extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    onSubmit = event => {
+
+    }
+
+    render(){
+        return(
+            <form onSubmit = {this.onSubmit}>
+
+            </form>
+        );
+        
+    }
+}
+
+export default SignUpForm;
