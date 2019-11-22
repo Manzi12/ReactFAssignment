8b224dc0e8d0ddcbae2b7e0fc397f5946bc32560
Manzi Joseph <manzeyjoseph@gmail.com>
2019-11-22 18:54:12 +0000

    Homepage made

---
+++ src/components/home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component{
+    render(){
+        return(
+            <div className="col-md-6">
+                <h1>YOU ARE HOME SAFE</h1>
+            </div>
+        )
+    }
+}
+
+export default Home;
--- stories/index.js
+++ stories/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Home from '../src/components/home';
 import Search from '../src/components/search';
 import SignUpForm from '../src/components/signup';
 import SignIn from '../src/components/signin';
+
+storiesOf("Home/Home",module).add("default",() => (
+<Home />
+));
 
 storiesOf("Search/Search",module).add("default",()=>(
     <Search/>
