417124cbe4e5face842a4960da10e1d7de16420e
Manzi Joseph <manzeyjoseph@gmail.com>
2019-12-08 20:34:51 +0000

    signup updated with firebase

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,95 +1,47 @@
-import React, { useCallback } from 'react';
+import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router-dom';
-import app from '../../config/firebase';
-//import SignIn from '../signin';
+import {withFirebase} from '../../config/firebase';
+import * as ROUTES from '../../routes';
 
+const initialState = {
+    email: '',
+    password: ''
+};
 
-// const SignUpPage = () => (
-//     <div>
-//         <h1>SignUp</h1>
-//         <SignUpForm />
-//     </div>
-// );
+class SignUpForm extends Component {
+    constructor(props){
+        super(props);
+        this.state = {...initialState};
+    }
 
-const SignUp = ({history}) => {
-    const handleSignUp = useCallback(async event =>{
-        event.preventDefault();
-        const { email,password} = event.target.elements;
-        try {
-            await app 
-            .auth()
-            .createUserWithEmailAndPassword(email.value,password.value);
-            history.push("/");
-        } catch (error){
-            alert(error);
-        }
-    },[history]);
+    onChange = event => {
+        this.setState({[event.target.name]: event.target.value})
+    };
 
+    onSubmit = event => {
+    const { email, password } = this.state;
 
-    
-    
-
-
-// const initialState = {
-//     username:'',
-//     email: '',
-//     password: ''
-// };
-
-// class SignUpForm extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {...initialState};
-//     }
-
-//     onChange = event => {
-//         this.setState({[event.target.name]: event.target.value})
-//     };
-
-//     onSubmit = event => {
-//     event.preventDefault();
-//     const { username, email, password } = this.state;
-
-//     this.props.firebase
-//       .signUpNewUserWithEmailAndPassword(email, password)
-//       .then(authUser => {
-//         this.setState({ ...initialState });
-//         this.props.history.push("/searchResults");
-//       })
-//       .catch(error => console.log(error));
-//   };
+    this.props.firebase
+      .doCreateUserWithEmailAndPassword(email, password)
+      .then(authUser => {
+        this.setState({ ...initialState });
+        this.props.history.push(ROUTES.SEARCH);
+      });
+      event.preventDefault();
+  };
 
 
 
-//     render(){
-//         const {
-//             username,
-//             email,
-//             password
-//         } = this.state;
+    render(){
+        const {
+            email,
+            password
+        } = this.state;
 
         return(
             <div>
-
-                <h1>Sign up for the account</h1>
-                <p>If you have no account please signup to use our private search</p>
-                {/* <Link to="/SignIn"><p>Already have an account</p></Link> */}
-
-            
-            <form onSubmit = {handleSignUp}>
-                <label>Username</label>
-                <input name="username" type="username" placeholder="Full Name" />
-                <label>Email</label>
-                <input name="email" type="email" placeholder="Email" />
-                <label>Password</label>
-                <input name="password" type="password" placeholder="Password" />
-                {/* name = "username"
-                value={username}
-                onChange = {this.onChange}
-                type = "text"
-                placeholder = "Full Name"
-                /> */}
-                 {/* <input
+            <form onSubmit = {this.onSubmit}>
+                 <input
                 name = "email"
                 value={email}
                 onChange = {this.onChange}
@@ -102,16 +54,23 @@ const SignUp = ({history}) => {
                 onChange = {this.onChange}
                 type = "password"
                 placeholder = "Password"
-                /> */}
+                /> }
 
                 <button type="submit">Sign Up</button>
 
             </form>
-
             </div>
         );
         
-    }
+    };
+};
 
-export default withRouter(SignUp);
-//export default SignUpForm;
+const SignUp = withRouter(withFirebase(SignUpForm));
+
+const SignUpSite = () => (
+    <Fragment>
+        <SignUp />
+    </Fragment>
+)
+
+export default SignUpSite;
