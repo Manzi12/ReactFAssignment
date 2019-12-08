978cb8df43d163f60c61897b466a0f8017c72887
Manzi Joseph <manzeyjoseph@gmail.com>
2019-12-08 20:03:37 +0000

    commented out the unsed link

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import {withFirebase} from '../../config/firebase';
 import { withRouter } from 'react-router-dom';
-import { Link} from 'react-router-dom';
 import * as ROUTES from '../../routes';
 
 
